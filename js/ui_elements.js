bc99e6cd5a4e8f86502d84d8116c685aca2c01ec
Eric Brown <eric.brown8787@gmail.com>
2023-10-16 13:24:42 -0400

    Create main screen layout

--- js/ui_elements.js
+++ js/ui_elements.js
@@ -1,6 +1,6 @@
 var UI = {
 
-    Button : function(x,y,text,params) {
+    Button: function (x, y, text, params) {
 
         //
         //Default Parameters
@@ -13,10 +13,10 @@ var UI = {
             params.backgroundColor = 0xEEEEEE;
         if (params.textStyle == undefined) {
             params.textStyle = {
-                fontFamily : 'Arial', 
-                fontSize: 24, 
-                fill : 0x000000
-            }
+                fontFamily: 'Arial',
+                fontSize: 24,
+                fill: 0x000000
+            };
         }
 
         //
@@ -24,24 +24,24 @@ var UI = {
         //
 
         var ourButton = new PIXI.Container();
-            ourButton.interactive = true;
-            ourButton.x = x;
-            ourButton.y = y;
+        ourButton.interactive = true;
+        ourButton.x = x;
+        ourButton.y = y;
 
         var buttonBody = new PIXI.Graphics();
-            buttonBody.beginFill(params.backgroundColor);
-            buttonBody.drawRect(0, 0, 200, 100);
-            ourButton.addChild(buttonBody);
+        buttonBody.beginFill(params.backgroundColor);
+        buttonBody.drawRect(0, 0, 100, 50);
+        ourButton.addChild(buttonBody);
 
-            ourButton.body = buttonBody;
-        
-        var buttonText = new PIXI.Text(text,params.textStyle);
-            buttonText.anchor.set(.5,.5);
-            buttonText.x = 100;
-            buttonText.y = 50;
-            ourButton.addChild(buttonText);
+        ourButton.body = buttonBody;
 
-            ourButton.label = buttonText;
+        var buttonText = new PIXI.Text(text, params.textStyle);
+        buttonText.anchor.set(.5, .5);
+        buttonText.x = buttonBody.width / 2;
+        buttonText.y = buttonBody.height / 2;
+        ourButton.addChild(buttonText);
+
+        ourButton.label = buttonText;
 
         //
         //Event Listeners
@@ -51,34 +51,34 @@ var UI = {
         ourButton.onclick = function (e) {
             console.log("Clicked");
         };
-        ourButton.on("click",(e) => ourButton.onclick(e));
-        
+        ourButton.on("click", (e) => ourButton.onclick(e));
+
         //Pointer down and up
         ourButton.pointerdown = function (e) {
             ourButton.alpha = 0.7;
         };
         ourButton.on("pointerover", (e) => ourButton.pointerdown(e));
-        
+
         ourButton.pointerup = function (e) {
             ourButton.alpha = 1.0;
         };
-        ourButton.on("pointerout",(e) => ourButton.pointerup(e));
+        ourButton.on("pointerout", (e) => ourButton.pointerup(e));
 
         //Hover listeners
         ourButton.pointerover = function (e) {
             ourButton.alpha = 0.9;
         };
-        ourButton.on("pointerover",(e) => ourButton.pointerover(e));
-        
+        ourButton.on("pointerover", (e) => ourButton.pointerover(e));
+
         ourButton.pointerout = function (e) {
             ourButton.alpha = 1.0;
         };
-        ourButton.on("pointerout",(e) => ourButton.pointerout(e));
+        ourButton.on("pointerout", (e) => ourButton.pointerout(e));
 
         return ourButton;
 
     },
 
-    
 
-}
+
+};
---
+++ notes.md
@@ -0,0 +1,29 @@
+### Fish 1:
+
+Name:
+
+Species(Common): Buoyant Air Gulper
+
+Species(Binomial):
+
+Description: This fish can't actually breathe underwater, but doesn't want anyone to know. If tickled, it will lose its composure and swim to the surface to breathe.
+
+### Fish 2:
+
+Name:
+
+Species(Common): Void-Hopping Singleton
+
+Species(Binomial):
+
+Description: A multiversal constant, only one individual of this species may be born in each timeline. A school of these fish is made up entirely of versions of the same individual, each one a refugee from a different doomed reality.
+
+### Fish 3:
+
+Name:
+
+Species(Common): Abyssal Shrieker
+
+Species(Binomial):
+
+Description: This fish is cursed with knowledge of the invisible barrier that separates our world from its own. Vibrations in this barrier briefly attract the fish's attention, and with it, its rage.
