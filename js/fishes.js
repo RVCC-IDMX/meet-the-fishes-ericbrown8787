ea0635aefaf8a18403329e26fb30f1d9af1a2274
Eric Brown <eric.brown8787@gmail.com>
2023-10-24 21:57:37 -0400

    Update fonts and add more animations

--- js/fishes.js
+++ js/fishes.js
@@ -17,14 +17,26 @@ async function bob(fish) {
   });
 }
 
-async function pace(fish) {
-
+async function flip(fish) {
   await Animate.to(fish, {
-    scale: { x: fish.scale.x * -1, y: fish.scale.y },
-    duration: 100
+    rotation: Math.PI,
+    duration: 500,
+    easing: Animate.easeInOut,
   });
   await Animate.to(fish, {
-    x: app.view.width * .75 + 50,
+    rotation: 0,
+    duration: 500,
+    easing: Animate.easeInOut,
+  });
+}
+
+async function pace(fish) {
+  await Animate.to(fish, {
+    scale: { x: fish.scale.x * -1, y: fish.scale.y },
+    duration: 100,
+  });
+  await Animate.to(fish, {
+    x: app.view.width * 0.75 + 50,
     duration: 500,
     easing: Animate.easeOut,
   });
@@ -33,57 +45,53 @@ async function pace(fish) {
     duration: 100,
   });
   await Animate.to(fish, {
-    x: app.view.width * .75 - 50,
+    x: app.view.width * 0.75 - 50,
     duration: 500,
     easing: Animate.easeOut,
   });
-
 }
-
-
-
 
 const fishes = [
   {
-    name: "inky",
+    name: 'inky',
     sprite: {
-      image: "fish1.png",
+      image: 'fish1.png',
       scale: 0.15,
-      animation: bob,
+      animation: flip,
     },
     species: {
-      common: "Buoyant Aether Gulper",
-      scientific: "Chilomycterus haustus",
+      common: 'Buoyant Aether Gulper',
+      scientific: 'Chilomycterus haustus',
     },
     description:
       "This fish can't actually breathe underwater, but doesn't want anyone to know. If tickled, it will lose its composure and swim to the surface to breathe.",
     background: { color: 0x234030, noise: { noise: 0.67, seed: 60 } },
   },
   {
-    name: "blinky",
+    name: 'blinky',
     sprite: {
-      image: "fish2.png",
+      image: 'fish2.png',
       scale: 0.3,
-      animation: bob
+      animation: bob,
     },
     species: {
-      common: "Plane-Hopping Singleton",
-      scientific: "Xenolepomis solitus",
+      common: 'Plane-Hopping Singleton',
+      scientific: 'Xenolepomis solitus',
     },
     description:
-      "A multiversal constant, only one individual of this species may be born in each timeline. A school of these fish is made up entirely of versions of the same individual, each one a refugee from a different doomed reality.",
+      'A multiversal constant, only one individual of this species may be born in each timeline. A school of these fish is made up entirely of versions of the same individual, each one a refugee from a different doomed reality.',
     background: { color: 0x1159ff, noise: { noise: 0.4, seed: 180 } },
   },
   {
-    name: "clyde",
+    name: 'clyde',
     sprite: {
-      image: "fish3.png",
+      image: 'fish3.png',
       scale: 0.2,
-      animation: pace
+      animation: pace,
     },
     species: {
-      common: "Abyssal Shrieker",
-      scientific: "Betta stridens",
+      common: 'Abyssal Shrieker',
+      scientific: 'Betta stridens',
     },
     description:
       "This fish is cursed with knowledge of the invisible barrier that separates our world from its own. Vibrations in this barrier briefly attract the fish's attention, and with it, its rage.",
